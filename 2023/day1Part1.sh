ba3ea99a87642ea32949246cb049d9c3fc22377c
Joe Leveille <joeleve212@gmail.com>
2024-11-21 18:44:08 -0700

    Complete day1 part1: 55130

--- 2023/day1Part1.sh
+++ 2023/day1Part1.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-numLimitedLines=$(grep -o "[0-9].*[0-9]" ./day1Input.txt)
+total=0
+numLimitedLines=$(grep -o "[0-9].*" ./day1Input.txt)
+
+
 for line in $numLimitedLines; do
-    firstNum=$(grep -o "^." $line)
-    lastNum=$(grep -o ".$" $line)
-    echo $firstNum$lastNum
+    line=$(echo $line | grep -o ".*[0-9]")
+    # echo $line               #only for debug
+    firstNum=$(echo $line | grep -o "^.")
+    lastNum=$(echo $line | grep -o ".$")
+    # echo $firstsNum$lastNum  #only for debug
+    total=$(($total + $firstNum$lastNum ))
+done
 
-
-
+echo $total
 
 
 
